16888f5df006b24968a1da95c2e5bc35dd5ddba3
Brian Zimmer <bzimmer@ziclix.com>
2020-12-30 16:37:48 -0800

    added centuries to totals

--- pkg/strava/analysis/passes/totals/totals.go
+++ pkg/strava/analysis/passes/totals/totals.go
@@ -12,13 +12,20 @@ import (
 
 const Doc = ``
 
+type Centuries struct {
+	Metric   int `json:"metric"`
+	Imperial int `json:"imperial"`
+}
+
 type Result struct {
 	Distance      unit.Length   `json:"distance"`
 	ElevationGain unit.Length   `json:"elevation_gain"`
 	MovingTime    time.Duration `json:"moving_time"`
+	Centuries     Centuries     `json:"centuries"`
 }
 
 func Run(ctx context.Context, pass *analysis.Pass) (interface{}, error) {
+	var cen Centuries
 	var dst, elv float64
 	var dur time.Duration
 	strava.EveryActivityPtr(func(act *strava.Activity) bool {
@@ -31,12 +38,19 @@ func Run(ctx context.Context, pass *analysis.Pass) (interface{}, error) {
 			dst = dst + act.Distance.Miles()
 			elv = elv + act.ElevationGain.Feet()
 		}
+		if act.Distance.Kilometers() >= 100.0 {
+			cen.Metric++
+		}
+		if act.Distance.Miles() >= 100.0 {
+			cen.Imperial++
+		}
 		return true
 	}, pass.Activities)
 	return &Result{
 		Distance:      unit.Length(dst),
 		ElevationGain: unit.Length(elv),
 		MovingTime:    dur / (time.Second * 60 * 60),
+		Centuries:     cen,
 	}, nil
 }
 
